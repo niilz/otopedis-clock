db134c40ca78512625646be307508cf87951b5cc
niilz <niilz@outlook.de>
2023-10-17 14:16:13 +0200

    Update clock with request animation frame

--- main.js
+++ main.js
@@ -3,15 +3,26 @@ const hourEl = document.getElementById("hour");
 const minEl = document.getElementById("min");
 const secEl = document.getElementById("sec");
 
-function clock() {
-  const now = new Date();
-  const hour = now.getHours();
-  const min = now.getMinutes();
-  const sec = now.getSeconds();
+let lastUpdate = 0;
 
-  hourEl.textContent = hour;
-  minEl.textContent = min;
-  secEl.textContent = sec;
+function clock() {
+  const updateClock = (timeStamp) => {
+    if (timeStamp - lastUpdate > 1000) {
+      const now = new Date();
+      const hour = now.getHours();
+      const min = now.getMinutes();
+      const sec = now.getSeconds();
+
+      hourEl.textContent = hour;
+      minEl.textContent = min;
+      secEl.textContent = sec;
+
+      lastUpdate = timeStamp;
+    }
+    requestAnimationFrame(updateClock);
+  };
+
+  requestAnimationFrame(updateClock);
 }
 
 clock();

6d423df24b447557b34dd0d0cb084323d2bf399d
niilz <niilz@outlook.de>
2023-10-17 14:05:58 +0200

    Basic date retrieval

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-function test() {
-  console.log("This is a test");
+const clockEl = document.getElementById("clock");
+const hourEl = document.getElementById("hour");
+const minEl = document.getElementById("min");
+const secEl = document.getElementById("sec");
+
+function clock() {
+  const now = new Date();
+  const hour = now.getHours();
+  const min = now.getMinutes();
+  const sec = now.getSeconds();
+
+  hourEl.textContent = hour;
+  minEl.textContent = min;
+  secEl.textContent = sec;
 }
 
-test();
+clock();
